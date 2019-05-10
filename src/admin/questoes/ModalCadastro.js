48e80ba7a74f0e5d6e783a1c1f6ac212fd5e7771
unknown <willnt@gmail.com>
2019-05-10 15:29:18 -0300

    a

--- src/admin/questoes/ModalCadastro.js
+++ src/admin/questoes/ModalCadastro.js
@@ -111,32 +111,6 @@ const img = {
     height: 'auto'
 }
 
-/*
-const thumb = {
-    display: 'inline-flex',
-    borderRadius: 2,
-    border: '1px solid #eaeaea',
-    marginBottom: 8,
-    marginRight: 8,
-    width: 'auto',
-    height: 150,
-    padding: 4,
-    boxSizing: 'border-box'
-}
-
-const thumbInner = {
-    display: 'flex',
-    minWidth: 0,
-    overflow: 'hidden'
-}
-
-const img = {
-    display: 'block',
-    width: 'auto',
-    height: '100%'
-}
-*/
-
 class ModalCadastro extends Component {
     state = {
         buttonAlternativas: true,
