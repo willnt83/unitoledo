f23cd763c8bcc59b9a29e8f522e1528c805ab359
Diego Dantas <diego.dantas@fmxsolucoes.com.br>
2019-07-02 20:59:44 -0300

    Atualização dash admin

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -3,7 +3,7 @@ import { Layout, Table, Row, Col, Icon, Progress } from 'antd'
 import { connect } from 'react-redux'
 import { withRouter } from "react-router-dom"
 import axios from "axios"
-
+import moment from 'moment'
 
 const { Content } = Layout
 
@@ -14,7 +14,7 @@ class Dashboard extends Component {
     }
 
     state = {
-        tableData: null,
+        tableData: [],
         tableLoading: false,
         showModal: false,
         simuladoId: null,
@@ -24,7 +24,17 @@ class Dashboard extends Component {
         dataDash: []
     }
 
+    compareByDates = (a, b) => {
+        a = moment(a, 'DD/MM/YYYY HH:mm')
+            b = moment(b, 'DD/MM/YYYY HH:mm')
+        if (a > b) return -1
+        if (a < b) return 1
+        return 0
+    }
+
+
     componentWillMount(){
+        this.setState({tableLoading: true})
         if(this.props.mainData === null || (this.props.contexto !== 'COORDENADOR' && this.props.contexto !== 'PROFESSOR' && this.props.contexto !== 'APPPROVA - ADMIN')){
             this.props.resetAll()
             window.location.replace("/app-prova")
@@ -70,68 +80,31 @@ class Dashboard extends Component {
 
         axios.post(this.props.backEndPoint+'/api/getDashAdmin', request)
         .then(res => {
-            console.log(res)
             var dataDash = res.data;
             this.setState({dataDash})
-            console.log(this.state.dataDash.total)
-            // var inicio = null
-            // var termino = null
-            // var situacao = null
-            // var alvos = []
-            // var tableData = []
-            // tableData = res.data.map(simulado => {
-            //     inicio = moment(simulado.dataHoraInicial)
-            //     termino = moment(simulado.dataHoraFinal)
-            //     situacao = (simulado.rascunho) ? 'Rascunho' : 'Público'
+            
+            var inicio = null
+            var termino = null
+            var tableData = []
+            tableData = res.data.list.map(list => {
+                inicio = moment(list.dataInicial)
+                termino = moment(list.dataFinal)
 
-            //     alvos = []
-            //     if(simulado.cursos && simulado.cursos.length > 0){
-            //         simulado.cursos.forEach(curso => {
-            //             alvos.push({
-            //                 key: curso.id,
-            //                 name: curso.nome,
-            //                 tipo: 'Curso'
-            //             })
-            //         })
-            //     }
+                return ({
+                    key: list.idSimulado,
+                    nome: list.nome,
+                    questoesRespondidas: list.questoesRespondidas,
+                    respostasCorretas: list.questoesCertas,
+                    percentual: list.percentual,
+                    dataInicio: inicio.format('DD/MM/YYYY HH:mm'),
+                    dataFim: termino.format('DD/MM/YYYY HH:mm'),
+                    inicioObj: inicio,
+                    terminoObj: termino
+                })
+            })
 
-            //     if(simulado.turmas && simulado.turmas.length > 0){
-            //         simulado.turmas.forEach(turma => {
-            //             alvos.push({
-            //                 key: turma.id,
-            //                 name: turma.nome,
-            //                 tipo: 'Turma'
-            //             })
-            //         })
-            //     }
-
-            //     if(simulado.disciplinas && simulado.disciplinas.length > 0){
-            //         simulado.disciplinas.forEach(disciplina => {
-            //             alvos.push({
-            //                 key: disciplina.id,
-            //                 name: disciplina.nome,
-            //                 tipo: 'Disciplina'
-            //             })
-            //         })
-            //     }
-
-            //     return ({
-            //         key: simulado.id,
-            //         nome: simulado.nome,
-            //         alvos: alvos,
-            //         questoes: simulado.questoes,
-            //         situacao: situacao,
-            //         rascunho: simulado.rascunho,
-            //         status: simulado.status,
-            //         inicio: inicio.format('DD/MM/YYYY HH:mm'),
-            //         termino: termino.format('DD/MM/YYYY HH:mm'),
-            //         inicioObj: inicio,
-            //         terminoObj: termino
-            //     })
-            // })
-
-            // this.setState({tableLoading: false})
-            // this.setState({tableData})
+            this.setState({tableLoading: false})
+            this.setState({tableData})
         })
         .catch(error =>{
             console.log('error: ', error)
@@ -139,105 +112,163 @@ class Dashboard extends Component {
         })
     }
     render(){
-        return(
-            <Content
-                style={{
-                    margin: "24px 16px",
-                    padding: 24,
-                    background: "#fff",
-                    minHeight: 280
-                }}
-            >
+        const columns = [
             {
-                 (this.state.dataDash && this.state.dataDash.total) ?
-                    <Row>
-                        <Col sm={24} md={6}>
-                            <Row type="flex">
-                                <Col md={24} align="center">
-                                    <Progress 
-                                        type="dashboard" 
-                                        gapDegree={360}
-                                        percent={1}
-                                        format={() => 
-                                            this.state.dataDash.total.totalSimulado ?
-                                            this.state.dataDash.total.totalSimulado
-                                            : 0
-                                        }
-                                    />
-                                </Col>
-                                <Col md={24} style={{fontSize: 17}} align="center">
-                                    Simulados Concluídos
-                                </Col>  
-                            </Row>
-                        </Col>
-                        <Col sm={24} md={6}>
-                            <Row type="flex">
-                                <Col md={24} align="center">
-                                    <Progress 
-                                        type="dashboard" 
-                                        gapDegree={360}
-                                        strokeColor="#FFA500"
-                                        percent={1}
-                                        format={() =>
-                                            this.state.dataDash.total.totalQuestoesRespondidas ?
-                                            this.state.dataDash.total.totalQuestoesRespondidas
-                                            : null
-                                        } 
-                                    />
-                                </Col>
-                                <Col md={24} align="center" style={{fontSize: 17}}>
-                                    Questões Respondidas
-                                </Col>  
-                            </Row>
-                        </Col>  
-                        <Col sm={24} md={6}>
-                            <Row type="flex">
-                                <Col md={24} align="center">
-                                    <Progress 
-                                        type="dashboard" 
-                                        strokeColor="#006400"
-                                        gapDegree={360}
-                                        percent={1}
-                                        format={() =>
-                                            this.state.dataDash.total.totalQuestoesCertas ?
-                                            this.state.dataDash.total.totalQuestoesCertas
-                                            : 0
-                                        } 
-                                    />
-                                </Col>
-                                <Col md={24} align="center" style={{fontSize: 17}}>
-                                    Questões Certas
-                                </Col>  
-                            </Row>
-                        </Col>  
-                        <Col sm={24} md={6}>
-                            <Row type="flex">
-                                <Col md={24} align="center">
-                                    <Progress 
-                                        type="circle" 
-                                        percent={
-                                            this.state.dataDash.total ?
-                                            this.state.dataDash.total.totalPercentual
-                                            : null
-                                        } 
-                                    />
-                                </Col>
-                                <Col md={24} align="center" style={{fontSize: 17}}>
-                                    Percentual de acerto
-                                </Col>  
-                            </Row>
-                        </Col>    
-                    </Row> 
-                
-                
-                :
-                    <Row>
-                        <Col span={24}>
-                            Nenhum simulado disponível.
-                        </Col>
-                    </Row>
-            }
-          </Content>
+                title: 'ID',
+                dataIndex: 'key',
+                sorter: (a, b) => a.key - b.key
+            },
+            {
+				title: "Simulado",
+				dataIndex: "nome",
+				sorter: (a, b) => { return a.nome.localeCompare(b.nome)},
+            },
+            {
+				title: "Iniciou em",
+                dataIndex: "dataInicio",
+                align: "center",
+				sorter: (a, b) => this.compareByDates(a.dataInicio, b.dataInicio)
+            },
+            {
+				title: "Finalizou em",
+                dataIndex: "dataFim",
+                align: "center",
+				sorter: (a, b) => this.compareByDates(a.dataFim, b.dataFim)
+            },
+            {
+                title: 'Questões Respondidas',
+                dataIndex: 'questoesRespondidas',
+                align: "center",
+                sorter: (a, b) => a.questoesRespondidas - b.questoesRespondidas
+            },
+            {
+                title: 'Respostas Corretas',
+                dataIndex: 'respostasCorretas',
+                align: "center",
+                sorter: (a, b) => a.respostasCorretas - b.respostasCorretas
+            },
+            {
+                title: 'Percentual',
+                dataIndex: 'percentual',
+                align: "center",
+                sorter: (a, b) => a.percentual - b.percentual
+            },
+        ]
+
+        return(
+            <React.Fragment>
+                <Content
+                    style={{
+                        margin: "24px 16px",
+                        padding: 24,
+                        background: "#fff"
+                    }}
+                >
+                {
+                     (this.state.dataDash && this.state.dataDash.total) ?
+                        <Row>
+                            <Col sm={24} md={6}>
+                                <Row type="flex">
+                                    <Col md={24} align="center">
+                                        <Progress 
+                                            type="dashboard" 
+                                            gapDegree={360}
+                                            percent={1}
+                                            format={() => 
+                                                this.state.dataDash.total.totalSimulado ?
+                                                this.state.dataDash.total.totalSimulado
+                                                : 0
+                                            }
+                                        />
+                                    </Col>
+                                    <Col md={24} style={{fontSize: 17}} align="center">
+                                        Simulados Concluídos
+                                    </Col>  
+                                </Row>
+                            </Col>
+                            <Col sm={24} md={6}>
+                                <Row type="flex">
+                                    <Col md={24} align="center">
+                                        <Progress 
+                                            type="dashboard" 
+                                            gapDegree={360}
+                                            strokeColor="#FFA500"
+                                            percent={1}
+                                            format={() =>
+                                                this.state.dataDash.total.totalQuestoesRespondidas ?
+                                                this.state.dataDash.total.totalQuestoesRespondidas
+                                                : null
+                                            } 
+                                        />
+                                    </Col>
+                                    <Col md={24} align="center" style={{fontSize: 17}}>
+                                        Questões Respondidas
+                                    </Col>  
+                                </Row>
+                            </Col>  
+                            <Col sm={24} md={6}>
+                                <Row type="flex">
+                                    <Col md={24} align="center">
+                                        <Progress 
+                                            type="dashboard" 
+                                            strokeColor="#006400"
+                                            gapDegree={360}
+                                            percent={1}
+                                            format={() =>
+                                                this.state.dataDash.total.totalQuestoesCertas ?
+                                                this.state.dataDash.total.totalQuestoesCertas
+                                                : 0
+                                            } 
+                                        />
+                                    </Col>
+                                    <Col md={24} align="center" style={{fontSize: 17}}>
+                                        Questões Certas
+                                    </Col>  
+                                </Row>
+                            </Col>  
+                            <Col sm={24} md={6}>
+                                <Row type="flex">
+                                    <Col md={24} align="center">
+                                        <Progress 
+                                            type="circle" 
+                                            percent={
+                                                this.state.dataDash.total ?
+                                                this.state.dataDash.total.totalPercentual
+                                                : null
+                                            } 
+                                        />
+                                    </Col>
+                                    <Col md={24} align="center" style={{fontSize: 17}}>
+                                        Percentual de acerto
+                                    </Col>  
+                                </Row>
+                            </Col>    
+                        </Row> 
+
+                                        
+                    :
+                        <Row>
+                            <Col span={24}>
+                                Nenhum simulado disponível.
+                            </Col>
+                        </Row>
+                }
+                </Content>
+                <Content
+                    style={{
+                        margin: "16px 16px",
+                        padding: 24,
+                        background: "#fff"
+                    }}
+                >
+                    <Table
+                        columns={ columns } 
+                        dataSource={ this.state.tableData }
+                        loading={this.state.tableLoading}
+                        scroll={{x: 1000}}
+                    />
+                </Content>
+            </React.Fragment>
         )
     }
 }
